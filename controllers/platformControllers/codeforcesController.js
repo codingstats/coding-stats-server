ad7c73a5bd5b38b81cc3f00d9e13fd4fd410b841
JaTin <jigyashusaini7@gmail.com>
2024-03-15 20:37:30 +0530

    changing env to production

--- controllers/platformControllers/codeforcesController.js
+++ controllers/platformControllers/codeforcesController.js
@@ -32,7 +32,7 @@ exports.getUserDetails = catchAsync(async (req, res, next) => {
 
     const browser = await puppeteer.launch({
         args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote",],
-        executablePath: process.env.NODE_ENV === "development" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+        executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
